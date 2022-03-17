6dd1c1f7a53f05e125f195950bf1caad33af1833
danyow <i.zuucol@gmail.com>
2022-03-17 17:04:42 +0800

    第一次提交

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # unity-doc-to-md
 把 unity 的 html 文档转为 md 格式
+
+## 下载源文件
+
+[2020.3](https://storage.googleapis.com/localized_docs/zh-cn/2020.3/UnityDocumentation.zip)
+
+## 创建四个文件夹
+
+`unity/zh_2020`
+`unity/zh_anchor`
+`unity/html2md_temp`
+`unity/docs`
+
+## 确保自己的 `nodejs` 环境为 `linux`, 并解开限制
+
+`ulimit -n 10240`
+
+## 运行代码
---
+++ scripts/html2md_lua_doc.js
@@ -0,0 +1,234 @@
+/**
+ * https://storage.googleapis.com/localized_docs/zh-cn/2021.2/UnityDocumentation.zip
+ *
+ *
+ *
+ */
+
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+const TService = require("turndown")
+const TPlugin = require('turndown-plugin-gfm')
+const os = require("os");
+
+const tds = new TService({
+  headingStyle: "atx",
+  bulletListMarker: "-",
+  codeBlockStyle: "fenced",
+  emDelimiter: "*",
+  linkStyle: "referenced",
+  linkReferenceStyle: "full",
+})
+tds.use(TPlugin.gfm)
+tds.use([TPlugin.tables, TPlugin.strikethrough])
+// tds.addRule('pre2Code', {
+//   filter: ['pre'],
+//   replacement(content) {
+//     const len = content.length
+//     // 除了pre标签，里面是否还有code标签包裹，有的话去掉首尾的`（针对微信文章）
+//     const isCode = content[0] === '`' && content[len - 1] === '`'
+//     const result = isCode ? content.substr(1, len - 2) : content
+//     return '```\n' + result + '\n```\n'
+//   }
+// })
+
+//要遍历的文件夹所在的路径
+let DIR_EN = path.resolve('../../../lua/lua53doc/')
+let DIR_AC = path.resolve('../../../lua/lua53doc_anchor/')
+let TEMP = path.resolve('../../../lua/lua53doc_temp/')
+let DIR_MD = path.resolve('../../../lua/lua53doc_md/')
+
+let fileNames = []
+
+const anchors = {}
+
+getAllAnchor(DIR_EN, DIR_AC)
+
+/**
+ * 把所有字母小写
+ * @param title
+ */
+function toAnchor(title) {
+  let anchor = title.replaceAll(/\w+/g, function (rep) {
+    return rep.toLocaleLowerCase()
+  })
+  // 把所有空格变成-
+  anchor = anchor.replaceAll(/\s+/g, function () {
+    return '-'
+  })
+  // 把 # ( ) . / : 换成 空
+  anchor = anchor.replaceAll(/[#().\/:]+/g, function () {
+    return ''
+  })
+  return anchor
+}
+
+/**
+ * 获取所有锚点
+ * @param sourceDir
+ * @param tempDir
+ */
+function getAllAnchor(sourceDir, tempDir) {
+
+  let fileInfos = fs.readdirSync(sourceDir)
+  if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir)
+  }
+  for (let index = 0; index < fileInfos.length; index++) {
+    let fileInfo = fileInfos[index]
+    // 获取绝对路径得到 states
+    const filePath = path.join(sourceDir, fileInfo)
+    const tempPath = path.join(tempDir, fileInfo)
+    const fileName = path.basename(fileInfo, path.extname(fileInfo))
+    let states = fs.statSync(filePath)
+    if (states.isFile() && fileInfo.endsWith('.html')) {
+      if (fileNames.length !== 0 && !fileNames.includes(fileName)) {
+        continue
+      }
+      let content = fs.readFileSync(filePath, 'utf8')
+      content = content.replaceAll(/<a href="#(.+)">(.+)<\/a>/g, function (rep, $1, $2) {
+        if (!anchors[fileName]) {
+          anchors[fileName] = {}
+        }
+        anchors[fileName][$1] = toAnchor($2)
+        return rep
+      })
+      content = content.replaceAll(/<a name="(.+)">.+[\r\n]+<h\d>(.+)<\/h\d>/g, function (rep, $1, $2) {
+        if (!anchors[fileName]) {
+          anchors[fileName] = {}
+        }
+        anchors[fileName][$1] = toAnchor($2)
+        return rep
+      })
+      fs.writeFileSync(tempPath, content)
+    } else if (states.isDirectory()) {
+      getAllAnchor(filePath, tempPath)
+    }
+  }
+}
+
+
+//调用文件遍历方法
+readDirectory(DIR_AC, TEMP, DIR_MD)
+
+/**
+ * 文件遍历方法
+ * @param sourceDir 需要遍历的文件路径
+ * @param tempDir 放到哪个文件夹
+ * @param destDir 放到哪个文件夹
+ */
+function readDirectory(sourceDir, tempDir, destDir) {
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir)
+  }
+  if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir)
+  }
+  let fileInfos = fs.readdirSync(sourceDir)
+  for (let index = 0; index < fileInfos.length; index++) {
+    let fileInfo = fileInfos[index]
+    // 获取绝对路径得到 states
+    const filePath = path.join(sourceDir, fileInfo)
+    const tempPath = path.join(tempDir, fileInfo)
+    const destPath = path.join(destDir, fileInfo.replace(".html", ".md"))
+    const fileName = path.basename(fileInfo, path.extname(fileInfo))
+    let states = fs.statSync(filePath)
+    if (states.isFile() && fileInfo.endsWith('.html')) {
+      if (fileNames.length !== 0 && !fileNames.includes(fileName)) {
+        continue
+      }
+      // 按行读取文件
+      let readStream = fs.createReadStream(filePath, "utf8")
+      if (!fs.existsSync(tempPath)) {
+        fs.writeFileSync(tempPath, '')
+      }
+      let writeStream = fs.createWriteStream(tempPath)
+      let reader = readline.createInterface(readStream)
+      let isStart = true
+      reader.on('line', function (line) {
+        // if (line.includes('<div class="nextprev clear">')) {
+        //   isStart = false
+        // }
+        // if (line.includes('<h1>')) {
+        //   isStart = true
+        // }
+        if (isStart) {
+          writeStream.write(line + os.EOL)
+        }
+      })
+      reader.on('close', function () {
+        writeStream.close(function () {
+          let html = fs.readFileSync(tempPath).toString()
+          let md = tds.turndown(html)
+
+          md = md.replaceAll('contents.html', 'contents.md')
+          md = md.replaceAll('glossary.html', 'glossary.md')
+          md = md.replaceAll('index.html', 'index.md')
+          md = md.replaceAll('manual.html', 'manual.md')
+          md = md.replaceAll('logo.gif', 'http://cloudwu.github.io/lua53doc/logo.gif')
+          md = md.replaceAll('osi-certified-72x60.png', 'http://cloudwu.github.io/lua53doc/osi-certified-72x60.png')
+          md = md.replaceAll('<br>', '<br/>')
+          // md = md.replaceAll('../StaticFilesManual/', 'https://docs.unity3d.com/cn/2021.2/StaticFilesManual/')
+          // md = md.replaceAll('../ScriptReference/', 'https://docs.unity3d.com/cn/2021.2/ScriptReference/')
+          // md = md.replaceAll('../StaticFiles/', 'https://docs.unity3d.com/cn/2021.2/StaticFiles/')
+          // md = md.replaceAll('../uploads/', 'https://docs.unity3d.com/cn/2021.2/uploads/')
+          // md = md.replaceAll('https://docs.unity3d.com/Manual/', '')
+          // 把 `Manual` 文件内的文件格式转为 `md`
+          // md = md.replaceAll(/]: .+\.html/g, function (rep) {
+          //   return rep.replaceAll('.html', '.md')
+          // })
+          // 把 < > 转义
+          // md = md.replaceAll('<', '&lt;')
+          // md = md.replaceAll('>', '&gt;')
+          md = md.replaceAll(/(]: (.+\.md)?#)(.+)/g, function (rep, _, _, tag) {
+
+            let name = fileName
+            if (rep.includes('.md')) {
+              name = rep.replaceAll(/]: (.+)\.md.+/g, function (_, $1) {
+                return $1
+              })
+            }
+            rep = rep.replaceAll(/#[\w\-]+/g, function (_) {
+              // // 判断这个单词在不在不可分割的表里面
+              // for (let key in anchors) {
+              //   if (key.includes(tag)) {
+              //     tag = tag.replaceAll(key, anchors[key])
+              //   }
+              // }
+              //
+              // // 首字母小写但后面全大写
+              // tag = tag.replaceAll(/^[a-z][A-Z]+/g, function (rep) {
+              //   return rep.toLocaleLowerCase()
+              // })
+              // // 首字母大写但后面小写
+              // tag = tag.replaceAll(/[A-Z][a-z]+/g, function (rep) {
+              //   return rep.toLocaleLowerCase() + '-'
+              // })
+              // // 首字母为数字或者大写且后面大写
+              // tag = tag.replaceAll(/([0-9]|[A-Z])[A-Z]+/g, function (rep) {
+              //   return rep.toLocaleLowerCase() + '-'
+              // })
+              // // 如果最后一个字母是 - 就删除
+              // if (tag.endsWith('-')) {
+              //   tag = tag.substr(0, tag.length - 1)
+              // }
+              // // 修复错误
+              // if (tag.includes('--')) {
+              //   tag = tag.replaceAll('--', '-')
+              // }
+              if (anchors[name] && anchors[name][tag]) {
+                return '#' + anchors[name][tag]
+              }
+              return '#' + tag
+            })
+            return rep
+          })
+          fs.writeFileSync(destPath, md)
+        })
+      })
+    } else if (states.isDirectory()) {
+      readDirectory(filePath, tempPath, destPath)
+    }
+  }
+}
