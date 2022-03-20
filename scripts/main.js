eef4d26376b00ce0108a8b1ee82e7b618bf1c1c9
danyow <i.zuucol@gmail.com>
2022-03-20 08:44:05 +0800

    考虑bagpipe

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,6 @@ const path = require('path')
 const readline = require('readline')
 const TService = require("turndown")
 const TPlugin = require('turndown-plugin-gfm')
-const fileToPath = require('./fileToPath')
 const httpRequest = require('./httpRequest')
 const os = require("os");
 
@@ -29,9 +28,9 @@ const BASE_PATH = path.resolve('../unity_doc/', LANGUAGE, VERSION)
 
 //要遍历的文件夹所在的路径
 const ROOT_PATH = path.join(BASE_PATH, 'root')
-const ANCH_PATH = path.join(BASE_PATH, 'anchor')
 const TEMP_PATH = path.join(BASE_PATH, 'temp')
-const MARK_PATH = path.join(BASE_PATH, 'markdown')
+// const MARK_PATH = path.join(BASE_PATH, 'markdown')
+const MARK_PATH = path.resolve('../doc-unity-manual/docs/')
 
 // 专门测试某个文件
 const debugFiles = [
@@ -72,7 +71,7 @@ async function main() {
   let scriptList = toList(scriptData, 'ScriptReference')
 
   copyFiles(manualList, false)
-  // copyFiles(scriptList, true)
+  copyFiles(scriptList, true)
 
   // 对拷贝过后的文件目录转md
 }
@@ -100,13 +99,12 @@ function transformToAnchor(link) {
 }
 
 
+// 检查或者创建文件
 function checkAndWriteFile(file, content = '') {
   if (!fs.existsSync(path.dirname(file))) {
     fs.mkdirSync(path.dirname(file), {recursive: true})
   }
-  if (!fs.existsSync(file)) {
-    fs.writeFileSync(file, content)
-  }
+  fs.writeFileSync(file, content, {encoding: 'utf8'})
 }
 
 // 遍历 附带 前几次的数据 通过 callback 自定义
@@ -142,6 +140,7 @@ function toList(database, root) {
     const anchor_title = transformToAnchor(config.title)
     const anchor_links = getValues(list, (obj) => obj.anchor_link)
     const anchor_titles = getValues(list, (obj) => obj.anchor_title)
+    // TODO: 保存该文件的 锚点列表
     let data = {
       link: link,
       title: title,
@@ -171,6 +170,7 @@ function copyFiles(list, isScript = false) {
         let html = handleHtml(listData, isScript)
         if (html !== '') {
           let md = gfm.turndown(html)
+          md = handleMarkdown(md, listData, isScript)
           checkAndWriteFile(listData.target, md)
         }
       })
@@ -188,7 +188,7 @@ function writeTemp(listData, isScript, doneCallback) {
 
 
   checkAndWriteFile(listData.temp)
-  let readStream = fs.createReadStream(listData.source, 'utf-8')
+  let readStream = fs.createReadStream(listData.source, 'utf8')
   let writeStream = fs.createWriteStream(listData.temp)
   let reader = readline.createInterface(readStream)
   let isStart = false
@@ -231,6 +231,9 @@ function handleHtml(listData, isScript) {
     console.log('预处理Html为空', listData)
     return html
   }
+  // 有时候会莫名起码多个\r进去
+  html = html.replaceAll('\r\n', '\n')
+
   if (isScript) {
     html = html.replaceAll('<a href="" class="switch-link gray-btn sbtn left hide">切换到手册</a>', '')
     html = html.replaceAll('<pre class="codeExampleCS">', '<pre class="codeExampleCS"> {{CODE-START}}')
@@ -281,4 +284,133 @@ function handleHtml(listData, isScript) {
     return rep
   })
   return html
+}
+
+
+function handleMarkdown(md, listData, isScript) {
+  // 对源路径映射
+  md = md.replaceAll('../StaticFilesManual/', BASEURL + '/StaticFilesManual/')
+  md = md.replaceAll('../StaticFiles/', BASEURL + '/StaticFiles/')
+  md = md.replaceAll('../uploads/', BASEURL + '/uploads/')
+
+  // // 对 源路径 不同替换
+  // if (isScript) {
+  //   md = md.replaceAll('https://docs.unity3d.com/ScriptReference/', '')
+  //   md = md.replaceAll('../ScriptReference/', '')
+  //   md = md.replaceAll('https://docs.unity3d.com/Manual/', baseUrl + '/Manual/')
+  //   md = md.replaceAll('../Manual/', baseUrl + '/Manual/')
+  //   // 但把搜索相关置换回来
+  //   // md = md.replaceAll('30_search', 'https://danyow.cn/search')
+  //   md = md.replaceAll('30_search', baseUrl + '/ScriptReference/30_search')
+  // } else {
+  //   md = md.replaceAll('https://docs.unity3d.com/ScriptReference/', baseUrl + '/ScriptReference/')
+  //   md = md.replaceAll('../ScriptReference/', baseUrl + '/ScriptReference/')
+  //   md = md.replaceAll('https://docs.unity3d.com/Manual/', '')
+  //   md = md.replaceAll('../Manual/', '')
+  //
+  //   md = md.replaceAll('30_search', baseUrl + '/Manual/30_search')
+  // }
+  // md = md.replaceAll('../ScriptReference/', 'https://docs.unity3d.com/cn/' + version + '/ScriptReference/')
+  // 把 `Manual` 引用带` html `转为 `md`
+  // md = md.replaceAll(/]: .+\.html/g, function (rep) {
+  //
+  //   // 如果里面的url 包括了 Manual 或者 ScriptReference 就不能转换为 md
+  //   if (rep.includes('/Manual/') || rep.includes('/ScriptReference/')) {
+  //     return rep
+  //   }
+  //   return rep.replaceAll('.html', '.md')
+  // })
+
+  // 转义 &
+  md = md.replaceAll('&amp;', '&')
+
+  // 把 < > 转义
+  if (!isScript) {
+    md = md.replaceAll('<', '&lt;')
+    md = md.replaceAll('>', '&gt;')
+  }
+  if (isScript) {
+    md = md.replaceAll('\\[', '[')
+    md = md.replaceAll('\\]', ']')
+  }
+
+  // \*\*(.*?)\*\*
+  // 对多余 \# 删除
+  md = md.replaceAll('\\# ', '')
+  // 对双下滑线的进行 ** 处理
+  md = md.replaceAll('\\_\\_', '**')
+  md = md.replaceAll(/\*\*(.*?)\*\*/g, function (rep) {
+    // 前后剔除空格后 最后面补一个空格
+    return rep.trim() + ''
+  })
+
+  // 对 ![](http://xxx.xx) -> ![xxx.xx](http://xxx.xx)
+  md = md.replaceAll(/\!\[\]\((.*?)\)/g, function (rep, $1, $2) {
+    let name = path.basename($1)
+    rep = rep.replaceAll('![]', '![' + name + ']')
+    return rep
+  })
+
+  // 替换
+  // md = md.replaceAll(/]: (.*?)\.md/g, function (rep, $1) {
+  //   if (Object.keys(mdNameModifyList).includes($1)) {
+  //     return rep.replaceAll($1, mdNameModifyList[$1])
+  //   }
+  //   return rep
+  // })
+
+  // md = md.replaceAll(/(]: (.+\.md)?#)(.+)/g, function (rep, $1, $2, tag) {
+  //
+  //   let name = fileName
+  //   if (rep.includes('.md')) {
+  //     name = rep.replaceAll(/]: (.+)\.md.+/g, function (_, $1) {
+  //       return $1
+  //     })
+  //     if (Object.keys(mdNameModifyList).includes(name)) {
+  //       name = mdNameModifyList[name]
+  //     }
+  //   }
+  //   rep = rep.replaceAll(/#[\w\-]+/g, function (_) {
+  //     // // 判断这个单词在不在不可分割的表里面
+  //     // for (let key in anchors) {
+  //     //   if (key.includes(tag)) {
+  //     //     tag = tag.replaceAll(key, anchors[key])
+  //     //   }
+  //     // }
+  //     //
+  //     // // 首字母小写但后面全大写
+  //     // tag = tag.replaceAll(/^[a-z][A-Z]+/g, function (rep) {
+  //     //   return rep.toLocaleLowerCase()
+  //     // })
+  //     // // 首字母大写但后面小写
+  //     // tag = tag.replaceAll(/[A-Z][a-z]+/g, function (rep) {
+  //     //   return rep.toLocaleLowerCase() + '-'
+  //     // })
+  //     // // 首字母为数字或者大写且后面大写
+  //     // tag = tag.replaceAll(/([0-9]|[A-Z])[A-Z]+/g, function (rep) {
+  //     //   return rep.toLocaleLowerCase() + '-'
+  //     // })
+  //     // // 如果最后一个字母是 - 就删除
+  //     // if (tag.endsWith('-')) {
+  //     //   tag = tag.substr(0, tag.length - 1)
+  //     // }
+  //     // // 修复错误
+  //     // if (tag.includes('--')) {
+  //     //   tag = tag.replaceAll('--', '-')
+  //     // }
+  //     if (anchors[name] && anchors[name][tag]) {
+  //       return '#' + anchors[name][tag]
+  //     }
+  //     return '#' + tag
+  //   })
+  //   return rep
+  // })
+
+  md = md.replaceAll('{{CODE-START}}', '```csharp')
+  md = md.replaceAll('{{CODE-END}}', '```')
+  // 对表格进行优化
+  md = md.replaceAll('{{BR}}', '<br/>')
+  // 对子属性的选项表格
+  md = md.replaceAll('|  | ', '|  -> ')
+  return md
 }
--- scripts/test.js
+++ scripts/test.js
@@ -19,104 +19,51 @@
 // // print(name)
 //
 //
-// let html = '<table>\n' +
-//   '<colgroup>\n' +
-//   '<col style="text-align:left;">\n' +
-//   '<col style="text-align:left;">\n' +
-//   '</colgroup>\n' +
-//   '\n' +
-//   '<thead>\n' +
-//   '<tr>\n' +
-//   '\t<th style="text-align:left;"><strong>Topic</strong></th>\n' +
-//   '\t<th style="text-align:left;"><strong>描述</strong></th>\n' +
-//   '</tr>\n' +
-//   '</thead>\n' +
-//   '\n' +
-//   '<tbody>\n' +
-//   '\n' +
-//   '\n' +
-//   '\n' +
-//   '<table>\n' +
-//   '<colgroup>\n' +
-//   '<col style="text-align:left;">\n' +
-//   '<col style="text-align:left;">\n' +
-//   '</colgroup>\n' +
-//   '\n' +
-//   '<tbody>\n' +
-//   '<tr>\n' +
-//   '\t<td style="text-align:left;"><strong>Set Active Scene</strong></td>\n' +
-//   '\t<td style="text-align:left;">This allows you to specify which scene new Game Objects are created/instantiated in. There must always be one scene marked as the active scene</td>\n' +
-//   '</tr>\n' +
-//   '<tr>\n' +
-//   '\n' +
-//   '\n' +
-//   '<table>\n' +
-//   '<colgroup>\n' +
-//   '<col style="text-align:center;">\n' +
-//   '<col style="text-align:center;">\n' +
-//   '<col style="text-align:center;">\n' +
-//   '<col style="text-align:left;">\n' +
-//   '<col style="text-align:left;">\n' +
-//   '</colgroup>\n' +
-//   '\n' +
-//   '<thead>\n' +
-//   '<tr>\n' +
-//   '\t<th style="text-align:center;">情况</th>\n' +
-//   '\t<th style="text-align:center;">生成根运动</th>\n' +
-//   '\t<th style="text-align:center;">Animator.applyRootMotion</th>\n' +
-//   '\t<th style="text-align:left;">2018.2</th>\n' +
-//   '\t<th style="text-align:left;">2018.3 和 2018.4 (LTS)</th>\n' +
-//   '</tr>\n' +
-//   '</thead>\n' +
-//   '\n' +
-//   '<tbody>\n' +
-//   '<tr>\n' +
-//   '\t<td style="text-align:center;">A</td>\n' +
-//   '\t<td style="text-align:center;">是</td>\n' +
-//   '\t<td style="text-align:center;">是</td>\n' +
-//   '\t<td style="text-align:left;">在根变换 (Root Transform) 上累计应用根运动。</td>\n' +
-//   '\t<td style="text-align:left;">与 2018.2 相同</td>\n' +
-//   '</tr>'
-//
-// html = html.replaceAll(/<colgroup>\n((.|\n)*?)<tbody>/g, function (rep, $1) {
-//   if ($1.includes('<thead>')){
-//     return rep
-//   }
-//   // 判断有多少行
-//
-//   let col_count = rep.match(/\n/g).length - 3
-//   if (col_count > 0) {
-//     let thead = '<thead>\n<tr>\n'
-//     for (let index = 0; index < col_count; index++) {
-//       if (index == 0) {
-//         thead += '\t<th style="text-align:left;"><strong>Topic</strong></th>\n'
-//       } else{
-//         thead += '\t<th style="text-align:left;"><strong>描述</strong></th>\n'
-//       }
-//     }
-//     thead += '</tr>\n</thead>\n\n\<tbody>\n'
-//     rep = rep.replaceAll('<tbody>', thead)
-//   }
-//
-//   return rep
-// })
-//
-//
+const fs = require('fs')
+
+let html = fs.readFileSync('E:\\nodejs\\unity_doc\\cn\\2022.1\\temp\\Manual\\unity-manual\\platform-specific\\windows-store\\windowsstore-faq.html').toString()
+
+html = html.replaceAll('\r\n', '\n')
+
+html = html.replaceAll(/\<colgroup\>\n((.|\n)*?)\<tbody\>/g, function (rep, $1) {
+  if ($1.includes('<thead>')) {
+    return rep
+  }
+  // 判断有多少行
+
+  let col_count = rep.match(/\n/g).length - 3
+  if (col_count > 0) {
+    let thead = '<thead>\n<tr>\n'
+    for (let index = 0; index < col_count; index++) {
+      if (index == 0) {
+        thead += '\t<th style="text-align:left;"><strong>Topic</strong></th>\n'
+      } else {
+        thead += '\t<th style="text-align:left;"><strong>描述</strong></th>\n'
+      }
+    }
+    thead += '</tr>\n</thead>\n\n\<tbody>\n'
+    rep = rep.replaceAll('<tbody>', thead)
+  }
+
+  return rep
+})
+
+console.log(html)
 
 
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  });
-}
-
-async function asyncCall() {
-  console.log('calling');
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-  // expected output: "resolved"
-}
-
-asyncCall();
+// function resolveAfter2Seconds() {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve('resolved');
+//     }, 2000);
+//   });
+// }
+//
+// async function asyncCall() {
+//   console.log('calling');
+//   const result = await resolveAfter2Seconds();
+//   console.log(result);
+//   // expected output: "resolved"
+// }
+//
+// asyncCall();
